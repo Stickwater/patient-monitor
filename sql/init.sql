5e83b1bd5bdd6e03e481f6ddd2a11b71478d93a5
yz <1490236668@qq.com>
2026-05-25 21:58:49 +0800

    更新SQL初始化脚本

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,9 @@
 -- 患者监护系统数据库初始化脚本
 -- 数据库: rjgc
 
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
 CREATE DATABASE IF NOT EXISTS `rjgc` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `rjgc`;
 
